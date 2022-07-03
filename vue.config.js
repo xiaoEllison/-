a9105f95bb8a1e633f94b9749757ffb281eacb05
Ellisonxiao <ellisonxiao@foxmail.com>
2022-07-03 11:17:43 +0800

    初始化文件

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 3000,
+      host: "localhost",
+    },
+  },
+});
